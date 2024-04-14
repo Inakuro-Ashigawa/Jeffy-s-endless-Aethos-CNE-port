2b7fdebf3928f0d2fed6df02740fbfe0534e4e30
Tedyes <tedyes987@gmail.com>
2024-04-14 12:20:35 +0800

    fixed more shit

--- data/stages/hell.hx
+++ data/stages/hell.hx
@@ -3,6 +3,9 @@ import hxvlc.flixel.FlxVideo;
 import flixel.util.FlxTimer;
 var cutscene = null;
 
+var hudTween:FlxTween;
+var gameTween:FlxTween;
+
 var path = "stages/underworld/";
 var path2 = "stages/underworld/phs2/";
 
@@ -14,7 +17,7 @@ function create() {
 	trace(Assets.getPath(Paths.file("videos/jeffscene.mp4")));
 
     defaultCamZoom = 0.7;
-    FlxG.camera.followLerp = 0.04;
+    FlxTween.tween(FlxG.camera, {zoom: 0.7}, 0.1, {ease: FlxEase.quadInOut});
     dad.x = -400;
     gf.x = 500;
     gf.y = 450;
@@ -42,7 +45,6 @@ function create() {
 
     axehouse = new FlxSprite(-700, -65);
     axehouse.frames = Paths.getFrames(path+"axehouse");
-    axehouse.animation.addByPrefix("idle", "chop", 24,false);
     axehouse.scale.set(2.5,2.5);
     insert(3, axehouse);
 
@@ -50,7 +52,7 @@ function create() {
     evil.scrollFactor.set(0.5, 1);
     evil.scale.set(0.5,0.5);
     insert(4, evil);
-
+    
     statir = new FlxSprite(-1000, -300).loadGraphic(Paths.image(path2+"statir"));
     statir.scrollFactor.set(1,1);
     insert(4, statir);
@@ -83,20 +85,42 @@ function create() {
     hills.scrollFactor.set(1,1);
     insert(11, hills);
 
-   for (i in [statir,clouds,backhil, bulk, glow, chop, glow2, hills]) i.alpha = 0.00000000001;
+    var index = members.indexOf(strumLines.members[2].characters[0]);
+    black = new FlxSprite(-500, -500);
+    black.makeSolid(FlxG.width * 2.3, FlxG.width * 2.3, 0xFF000000);
+    black.scrollFactor.set(0, 0);
+    insert(100000000, black); // dont fucking ask - tedyes
 
+   for (i in [statir,clouds,backhil, bulk, glow, chop, glow2, hills]) i.alpha = 0.00000000001;
 }
 
 function update() {	
         evil.alpha = Math.sin(curBeat / 4);
 }
 
+function ss(){
+        if(hudTween != null) {
+            hudTween.cancel();
+        }
+        if(gameTween != null) {
+            gameTween.cancel();
+        }
+        FlxG.camera.shake(0.01, 0.05);
+        camHUD.zoom += 0.01;
+        FlxG.camera.zoom += 0.01;
+        hudTween = FlxTween.tween(camHUD, {zoom: 1}, 0.5, {ease: FlxEase.quadOut});
+        gameTween = FlxTween.tween(FlxG.camera, {zoom: defaultCamZoom}, 0.5, {ease: FlxEase.quadOut});
+}
+
 //i swear to god inakuro!!!!! try pressing the tab key once in a while!!!!
 //ill do it for you *this* time, but next time....! - Bromaster :3
-function chop() {	
-	axehouse.animation.play("idle");
+function diehouse() {
+        axehouse.animation.addByPrefix('chop', 'chop', 24, false);	
+	axehouse.animation.play("chop");
 }
 function house2(){
+        camGame.followLerp = 0.04;
+        camGame.flash(0xFFFF0000,1);
 	for (i in [clouds,backhil, bulk, glow, chop, glow2, hills]) i.alpha = 1;
 	statir.alpha = .4;
 	for (i in [axehouse,hill2, hill,gf]) i.alpha = 0.0000000001;
@@ -104,22 +128,32 @@ function house2(){
 	defaultCamZoom = 0.6;
 }
 function Transition(){
-	FlxTween.tween(camGame, {alpha: 0.000000001}, 1, {ease: FlxEase.sineInOut});
 	FlxTween.tween(camHUD, {alpha: 0.000000001}, 1, {ease: FlxEase.sineInOut});
-	new FlxTimer().start(1, function(tmr:FlxTimer)
-		{   
-			for (i in [staticd,statir,clouds,backhil, bulk, glow, chop, glow2, hills]) i.alpha = 0.00000000001;
-		});
+	for (i in [staticd,statir,clouds,backhil, bulk, glow, chop, glow2, hills, boyfriend, dad]) FlxTween.tween(i, {alpha: 0.000000001}, 1, {ease: FlxEase.sineInOut});
+        FlxTween.tween(black, {alpha: 1}, 1, {ease: FlxEase.sineInOut});
 }
 function Transition2(){
 	FlxTween.tween(camHUD, {alpha: 1}, 1, {ease: FlxEase.sineInOut});
 }
+function starting(){
+        camGame.followLerp = 1;
+	FlxTween.tween(black, {alpha: 0}, 5, {ease: FlxEase.sineInOut});
+}
+function lockin(){
+	FlxTween.tween(FlxG.camera, {zoom: 1}, 2.5, {ease: FlxEase.expoIn});
+}
 function back(){
-	camGame.alpha = 1;
+	for (i in [boyfriend, dad]) i.alpha = 1;
+        black.alpha = 0.0000001;
 }
 //this one is mine!!!! - bromaster
 // :3
 function cutsc() {
+        camGame.flash(0xFFFF0000,0.05);
 	cutscene.play(); // :3
 	trace("cutscene!!!");
+}
+
+function finale() {
+
 }
